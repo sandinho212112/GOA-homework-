c89fb37576bda3fb7137db0a6d56d4c70fca5a59
sandro sul <*sandrosulava1@gmail.com>
2024-05-29 17:30:00 -0700

    commit

---
+++ day 5/homework.py
@@ -0,0 +1,40 @@
+#variables that contain my information
+name = "Sandro"
+surname = "Sulava"
+age = 15
+height = 1.84
+address = "Tbilisi"
+
+#display information
+print("name")
+print("surname")
+print("age")
+print("height")
+print("address")
+
+
+#create variables
+a = 21
+b = 29
+c = 11
+d = 15
+e = 5
+f = 8
+g = 92
+h = 51
+i = 33
+j = 3
+
+#addition
+addition_results = a+b+c+d+e+f+g+h+i+j 
+#substruction
+substruction_results = a-b-c-d-e-g-f-h-j-i
+#division
+division_results = a/b/c/d/e/f/g/h/i/j
+#multiplication
+multiplication_results = a*b*c*d*e*f*g*h*i*j
+#display results
+print(addition_results)
+print(substruction_results)
+print(division_results)
+print(multiplication_results)
---
+++ day 6/classwork.py
@@ -0,0 +1,11 @@
+a = input()
+b = input()
+print(int(a) + int(b))
+addition_results = a+b
+division_results = a/b
+substruction_results = a-b
+multiplication_results = a*b
+print(addition_results)
+print(division_results)
+print(substruction_results)
+print(multiplication_results)
