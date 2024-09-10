ce797f45e15dd7771b2c82e3b89b298d8671684b
sandro sul <*sandrosulava1@gmail.com>
2024-09-09 21:31:02 -0700

    commit

---
+++ day 26/homework/26.py
@@ -0,0 +1,22 @@
+#1) ივარჯიშეთ .upper() / .lower() / .capitalize() და ახსენით კომენტარის სახით თითოეული ფუნქციის დანიშნულება (ახსენით რა არის dot notation)
+name= "sandro"
+#Sandro
+print(name.upper())
+#sandro
+print(name.lower())
+#SANDRO
+print(name.capitalize())
+
+#2) ივარჯიშეთ წინა მასალაზე (append / pop / insert) კომენტარებით ახსენით თითოეული ფუნქციის დანიშნულება და გააკეთეთ 3-3-3 მაგალითი
+#append
+names = ["gela", "vano", "zvingo", "jeksona"]
+names.append["sandrika"]
+print(names)
+#pop
+names = ["gela", "vano", "zvingo", "jeksona"]
+names.pop()
+print(names)
+#insert
+names = ["gela", "vano", "zvingo", "jeksona"]
+names.insert(3, "tolika")
+print(names)
